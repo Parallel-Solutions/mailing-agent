bceb0ab82cf4dbec3c65ddc365005e32e6ea842b
civ <ffff06@yandex.ru>
2026-08-11 12:25:02 +0300

    fix: shorten Alembic revision id

--- migrations/versions/0048_delivery_attempt_email_validation.py
+++ migrations/versions/0048_delivery_attempt_email_validation.py
@@ -1,6 +1,6 @@
 """Store the send-time email validation snapshot on delivery attempts.
 
-Revision ID: 0048_delivery_attempt_email_validation
+Revision ID: 0048_delivery_email_validation
 Revises: 0047_rusender_fixed_warmup
 Create Date: 2026-08-11
 """
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 from alembic import op
 
 
-revision = "0048_delivery_attempt_email_validation"
+revision = "0048_delivery_email_validation"
 down_revision = "0047_rusender_fixed_warmup"
 branch_labels = None
 depends_on = None
